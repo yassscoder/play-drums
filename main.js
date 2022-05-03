5e615f66d5fefa5f7de3f47b76d8e7ac236d74e4
Claudia Campos <clau.acr@gmail.com>
2022-05-03 19:59:13 +0200

    All  buttons added & working

--- main.js
+++ main.js
@@ -3,12 +3,24 @@
 //const audioCrash = new Audio("./sounds/crash.wav");
 
 const crashBtn = document.querySelector("#crash-btn");
-const tomLowBtn = document.querySelector("#low-tom-btn");
+const hihatOpenBtn = document.querySelector("#low-tom-btn");
+const hihatCloseBtn = document.querySelector("#hihat-close-btn");
+const kickBtn = document.querySelector("#kick-btn");
+const rideBtn = document.querySelector("#ride-btn");
+const snareBtn = document.querySelector("#snare-btn");
+const tomHighBtn = document.querySelector("#tom-high-btn");
+const tomMidBtn = document.querySelector("#tom-mid-btn");
+const tomLowBtn = document.querySelector("#tom-low-btn");
 
 crashBtn.addEventListener("click", playSound);
-
+hihatOpenBtn.addEventListener("click", playSound);
+hihatCloseBtn.addEventListener("click", playSound);
+kickBtn.addEventListener("click", playSound);
+rideBtn.addEventListener("click", playSound);
+snareBtn.addEventListener("click", playSound);
+tomHighBtn.addEventListener("click", playSound);
+tomMidBtn.addEventListener("click", playSound);
 tomLowBtn.addEventListener("click", playSound);
-
 
 function playSound(event) {
   const targetButton = event.target;
