4781046804d18dde79e1d5fc39b5be810e2b5b1b
Claudia Campos <clau.acr@gmail.com>
2022-05-03 10:51:52 +0200

    first steps with playSound function

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
 "use strict";
+
+const audioCrash = new Audio("./sounds/crash.wav");
+
+const crashBtn = document.querySelector("#crash-btn");
+crashBtn.addEventListener("click", playSound);
+
+function playSound() {
+  audioCrash.play();
+}
