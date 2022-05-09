9cfd25b58bb51d78f9c4cbc40a2cb866e6cab6f4
Yas Min <yasmin.lorenzo.c14@gmail.com>
2022-05-09 21:12:21 +0200

    refactor(getSoundSource): switch field dataset to JS map with sound routes

--- main.js
+++ main.js
@@ -31,6 +31,17 @@ validInputKeys.set(74, "./sounds/tom-high.wav");
 validInputKeys.set(75, "./sounds/tom-mid.wav");
 validInputKeys.set(76, "./sounds/tom-low.wav");
 
+const soundSourceRoutes = new Map();
+soundSourceRoutes.set(crashBtn, "./sounds/crash.wav");
+soundSourceRoutes.set(hihatOpenBtn, "./sounds/hihat-open.wav");
+soundSourceRoutes.set(hihatCloseBtn, "./sounds/hihat-close.wav");
+soundSourceRoutes.set(kickBtn, "./sounds/kick.wav");
+soundSourceRoutes.set(rideBtn, "./sounds/ride.wav");
+soundSourceRoutes.set(snareBtn, "./sounds/snare.wav");
+soundSourceRoutes.set(tomHighBtn, "./sounds/tom-high.wav");
+soundSourceRoutes.set(tomMidBtn, "./sounds/tom-mid.wav");
+soundSourceRoutes.set(tomLowBtn, "./sounds/tom-low.wav");
+
 function getSoundSource(event) {
   let soundSource;
   const pressedKey = event.keyCode;
@@ -44,7 +55,7 @@ function getSoundSource(event) {
     throw error;
   } else {
     const targetButton = event.target;
-    soundSource = targetButton.dataset.sound;
+    soundSource = soundSourceRoutes.get(targetButton);
     return soundSource;
   }
 }
